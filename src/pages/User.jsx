1758aea1e27dff4ba3caefbafed690c67a89677b
Bloodraven <74134064+Deshan555@users.noreply.github.com>
2024-08-30 22:52:40 +0530

    feat: Add User component

---
+++ src/pages/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import { motion } from 'framer-motion';
+import fing1 from '../assets/fig-01.webp';
+import '../theme.css';
+
+const User = ({ darkMode }) => {
+    return (
+    <h1>
+        User
+    </h1>
+    );
+}
+
+export default User;
