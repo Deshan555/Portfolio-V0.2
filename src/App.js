ee53984c7ef490b73acc25cc9c9b32c3e107e904
Bloodraven <74134064+Deshan555@users.noreply.github.com>
2024-08-31 20:25:20 +0530

    feat: Add Experience and User components, and update styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './theme.css';
 import TechnologyStack from './pages/technologyStack';
 import GitHubRepos from './pages/GitHubRepos';
 import Profile from './pages/Profile';
+import Experience from './pages/Experiance';
+import User from './pages/User';
+import { CodeOutlined } from '@ant-design/icons';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -22,22 +25,25 @@ const App = () => {
   return (
     <div className={darkMode ? 'dark-mode' : 'light-mode'} style={{ height: '100%' }}>
       <div className="header" style={{ display: 'flex', justifyContent: 'space-between', padding: '0 20px', alignItems: 'center' }}>
-        <h4 className={darkMode ? 'dark-mode-text' : 'light-mode-text'}>Deshan Jayashanka</h4>
+        <h4 className={darkMode ? 'dark-mode-text' : 'light-mode-text'}><CodeOutlined /> Deshan Jayashanka</h4>
         <Menu mode="horizontal" defaultSelectedKeys={['1']} style={{ background: 'transparent' }}>
-          <Menu.Item key="1">Home</Menu.Item>
-          <Menu.Item key="2">About</Menu.Item>
-          <Menu.Item key="3">Contact</Menu.Item>
-          <Menu.Item key="4">
+          <Menu.Item key="2">
+            <span className={darkMode ? 'dark-mode-text' : 'light-mode-text'} style={{ color: 'white' }}>
+            Projects</span></Menu.Item>
+          <Menu.Item key="3">Resume</Menu.Item>
+          {/* <Menu.Item key="4">
             <Switch checked={darkMode} onChange={toggleTheme} size='small' />
-          </Menu.Item>
+          </Menu.Item> */}
         </Menu>
       </div>
       {/* PROFILE SECTION */}
-      <Profile />
+      <User darkMode={darkMode} />
+      {/* EXPERIENCE SECTION */}
+      <Experience theme={darkMode} />
       {/* TECHNOLOGY STACKS SECTION */}
-      <TechnologyStack />
+      <TechnologyStack theme={darkMode} />
       {/* PROJECTS SECTION */}
-      {/* <GitHubRepos username="deshan555" theme={darkMode}/> */}
+      <GitHubRepos username="deshan555" theme={darkMode}/>
 
       <Button onClick={scrollToTop}>Scroll to Top</Button>
 
