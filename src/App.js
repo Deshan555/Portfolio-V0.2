2a0c383a7e035e159c6018291421ed5346a3ec06
Bloodraven <74134064+Deshan555@users.noreply.github.com>
2024-09-13 21:46:49 +0530

    add modified package json file

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Experience from './pages/Experiance';
 import User from './pages/User';
 import Contact from './pages/Contact';
 import { CodeOutlined } from '@ant-design/icons';
+import { Link } from 'react-scroll';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -24,14 +25,22 @@ const App = () => {
   };
 
   return (
-    <div className={darkMode ? 'dark-mode' : 'light-mode'} style={{ height: '100%'}}>
+    <div className={darkMode ? 'dark-mode' : 'light-mode'} style={{ height: '100%' }}>
       <div className="header" style={{ display: 'flex', justifyContent: 'space-between', padding: '0 20px', alignItems: 'center' }}>
         <h4 className={darkMode ? 'dark-mode-text' : 'light-mode-text'}><CodeOutlined /> Deshan Jayashanka</h4>
         <Menu mode="horizontal" defaultSelectedKeys={['1']} style={{ background: 'transparent' }}>
           <Menu.Item key="2">
-          <span style={{ fontSize: '14px', color: 'white', fontWeight: 'bold' }} className='font-light'>
-            Projects</span></Menu.Item>
-          <Menu.Item key="3"><span style={{ fontSize: '14px', color: 'white', fontWeight: 'bold' }} className='font-light'>Experience</span></Menu.Item>
+            <span style={{ fontSize: '14px', color: 'white', fontWeight: 'bold' }} className='font-light'>
+              Projects
+            </span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="experience" smooth={true} duration={500}>
+              <span style={{ fontSize: '14px', color: 'white', fontWeight: 'bold' }} className='font-light'>
+                Experience
+              </span>
+            </Link>
+          </Menu.Item>
           {/* <Menu.Item key="4">
             <Switch checked={darkMode} onChange={toggleTheme} size='small' />
           </Menu.Item> */}
@@ -40,16 +49,15 @@ const App = () => {
       {/* PROFILE SECTION */}
       <User darkMode={darkMode} />
       {/* EXPERIENCE SECTION */}
-      <Experience theme={darkMode} />
+      <Experience theme={darkMode} id="experience" />
       {/* TECHNOLOGY STACKS SECTION */}
       <TechnologyStack theme={darkMode} />
       {/* PROJECTS SECTION */}
-      <GitHubRepos username="deshan555" theme={darkMode}/>
+      <GitHubRepos username="deshan555" theme={darkMode} />
       {/* CONTACT SECTION */}
       <Contact theme={darkMode} />
 
       {/* <Button onClick={scrollToTop}>Scroll to Top</Button> */}
-
     </div>
   );
 };
