10d0fb40a9fb494f861a9451d0611b1c0624e8a8
Bloodraven <74134064+Deshan555@users.noreply.github.com>
2024-09-14 16:20:14 +0530

    refactor: Remove commented code and unused imports in Experiance.jsx

--- src/pages/Experiance.jsx
+++ src/pages/Experiance.jsx
@@ -6,31 +6,22 @@ import '../theme.css';
 const Experience = ({theme}) => {
   const [activeProjects, setActiveProjects] = useState([]);
 
-//   useEffect(() => {
-//     const activeProjectsFetch = async () => {
-//       let projectsArray = data.map(project => project.name);
-//       setActiveProjects(projectsArray);
-//     };
-
-//     activeProjectsFetch();
-//   }, []);
-
   return (
     <div style={{ marginRight: '10%', marginLeft: '10%', marginTop: 30 }}>
-      <Row gutter={[16, 16]} justify="end">
+      <Row gutter={[16, 16]}>
         <span style={{ fontSize: 45, marginTop: '0px', fontWeight: 'bold' }} className='font-header'>
           Experience
         </span>
       </Row>
 
       <Steps
+       style={{ marginTop: 10 }}
         progressDot
         current={1}
         direction="vertical"
         items={data.experience.map((item, index) => ({
-          title: <span style={{ fontSize: 16, fontWeight: 'bold', color: 'white' }} className={theme ? 'dark-mode-text font-light' : 'light-mode-text font-light'}>{item.company} ({item.year})</span>,
+          title: <span style={{ marginTop: 5, fontSize: 16, fontWeight: 'bold', color: 'white' }} className={theme ? 'dark-mode-text font-light' : 'light-mode-text font-light'}>{item.company} ({item.year})</span>,
           description: <span style={{ fontSize: 13, color: 'white' }} className={theme ? 'dark-mode-text font-light' : 'light-mode-text font-light'}><b>{item.position}</b>: {item.description}</span>,
-        //   content: item.description,
         }))}
       />
     </div>
